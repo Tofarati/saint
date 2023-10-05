4900a6f33744d557f4d4aeb0c427b50f389f91da
Tofarati <118224524+Tofarati@users.noreply.github.com>
2023-10-05 08:33:29 -0500

    Add files via upload
    This file calculates how much you earn based on your payrate and hours worked

---
+++ tax rate.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double hours, rate, pay;
+	
+	// Get the number of hours worked.
+	cout << "How many hours did you work?";
+	cin>> hours;
+	
+	
+	cout << "How much do you get paid per hour?";
+	cin >> rate;
+	
+	
+	pay = hours*rate;
+	
+	
+	cout<< "You have earned $"<<pay<< endl;
+	return 0;
+}
